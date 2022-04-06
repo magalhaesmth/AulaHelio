2860f295841c31de9a1090d3237a1c1f11463dc2
magalhaesmth <matheusurbano2002@hotmail.com>
2022-04-05 21:15:47 -0300

    Adicionando variaveis

--- lib/primeiro.dart
+++ lib/primeiro.dart
@@ -1,9 +1,31 @@
+import 'dart:io';
+
 void minha_funcao() {
   //tipo nome = atribuicao
-  String nomeProduto = "Chocolate";
-  double precoProduto = 2.2;
-  int quantidadeProduto = 3;
 
-  print(
-      '''O nome do produto é $nomeProduto, o preço segundo Matheus é $precoProduto e vamos comprar $quantidadeProduto''');
+  String nomeProduto = "Chocolate";
+  double precoProduto = 10;
+  int quantidadeProduto = 1;
+  //definindo variaveis
+
+  String entradaPreco = "3.5";
+  double preco = double.parse(entradaPreco);
+  //convertendo string para double
+
+  String entradaQuantidade = '5';
+  int quantidade = int.parse(entradaQuantidade);
+  //convertendo string para int
+
+  String? descricao;
+  //criando variavel sem valor
+
+  var nomeProduto2 = "Chocolate"; //definindo uma variavel string
+  var quantidae2 = 23;
+  var preco2 = 34.5;
+  //definindo uma variavel dinamica
+
+  print('Informe o seu nome');
+  var nome = stdin.readLineSync();
+
+  print('''O meu nome é $nome''');
 }
